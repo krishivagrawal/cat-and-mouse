ef80d4deba9d4018cf898b6af088428b521fbbf1
krishivagrawal <79085044+krishivagrawal@users.noreply.github.com>
2021-03-30 09:44:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var cat;
+var mouse;
+
+function preload() {
+    //load the images here
+    mouseImg.loadImage(mouse1.png);
+    mouseImg.loadImage(mouse2.ng);
+    mouseImg.loadImage(mouse3.png);
+    mouseImg.loadImage(mouse4.png);  
+ 
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(0);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
